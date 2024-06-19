9c4265457b359103e86a740d19ef894f3e173247
Joseph Chan <joschan@yahoo.co.uk>
2024-06-19 21:21:04 +0000

    Delete unwanted codes

--- run.py
+++ run.py
@@ -1,7 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-
 # Spreadsheet weblink - Please click on it to see the read-only google sheet
 """
 https://docs.google.com/spreadsheets/d/1eRzudCSUTXbOoQNGQWoZAAvzvJGvzIE8t3iOZxcQmvc/edit?gid=334103161#gid=334103161
